ece79bb733d395fec878a751b1a55191d25e732d
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-03-28 10:49:10 +0100

    Exercise nr.4

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ print(text_string[5])
 
 
 # Exercise nr.3
-# Using negative index return the d character from the string.
+# Using negative index return the b character from the string.
 text_string = "Hello! My name is Nick, I am web developer."
-print(text_string[-10])
+print(text_string[-12])
+
+
+# Exercise nr.4
+# Insert a method in order to return the index of the I character from the string.
+text_string = "Hello! My name is Nick, I am web developer."
+print(text_string.index("I"))
+
