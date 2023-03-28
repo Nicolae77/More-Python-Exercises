f7deb48b8a16c3bff73975f40558668385642ce7
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-03-28 10:41:10 +0100

    Exercise nr.2

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # Exercise nr.1
-# Using negative index get the last character in the string.
+# Using negative index get the last character from the string.
 text_string = "Hello! My name is Nick, I am web developer."
 print(text_string[-1])
 
 
 # Exercise nr.2
+# Using positive index return exclamation mark from the string.
+text_string = "Hello! My name is Nick, I am web developer."
+print(text_string[5])
