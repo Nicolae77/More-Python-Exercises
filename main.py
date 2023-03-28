b5543507b2fee42b84d09a0547e56e0251d7da11
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-03-28 10:36:28 +0100

    First exercise

---
+++ main.py
@@ -0,0 +1,7 @@
+# Exercise nr.1
+# Using negative index get the last character in the string.
+text_string = "Hello! My name is Nick, I am web developer."
+print(text_string[-1])
+
+
+# Exercise nr.2
