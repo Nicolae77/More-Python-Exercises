b4a2a9e7b1892364240ffbeffc96bcdb93de2c0d
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-05-01 17:22:01 +0100

    Python exercise nr. 124

--- main.py
+++ main.py
@@ -891,3 +891,13 @@ else:
 print(sorted(x.values()))
 print(sorted(x.values())[-1])
 print(sorted(x.values())[-1][-1])
+
+
+#  Exercise nr.123
+# Write code that prints out True! if the largest key in the dictionary divided by the second largest key in the dictionary returns a remainder equal to the smallest key in the dictionary. Otherwise, print out False!
+x = {1: "Python", 2: "Java", 3: "Javascript", 4: "Ruby", 5: "Perl", 6: "C#", 7: "C++"}
+
+if sorted(x.keys())[-1] % sorted(x.keys())[-2] == sorted(x.keys())[0]:
+    print("True!")
+else:
+    print("False!")
