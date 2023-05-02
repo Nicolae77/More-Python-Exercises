6858db7e8d0e93c3b164af0c9262d11fd4da43ec
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-05-02 11:52:54 +0100

    Python exercise nr. 127

--- main.py
+++ main.py
@@ -926,3 +926,24 @@ elif y[0][4] == 5:
 else:
     print("None")
 
+
+#  Exercise nr.127
+# write code that prints out True! if the 3rd element of the 3rd range is less than 6, prints out False! if the 1st element of the second range is 5, and prints out None! otherwise.
+x = [list(range(5)), list(range(5,9)), list(range(1,10,3))]
+print(x[2][2])
+print(x[1][0])
+if x[2][2] < 6:
+    print("True!")
+elif x[1][0] == 5:
+    print("False!")
+else:
+    print("None!")
+
+
+
+
+
+
+
+
+
