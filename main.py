4a81dee2829a7bda07499d0d2351ca164726ed79
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-05-02 12:02:29 +0100

    Python exercise nr. 129

--- main.py
+++ main.py
@@ -954,6 +954,18 @@ else:
     print("None")
 
 
+#  Exercise nr.129
+# Write code that prints out True! if the length of the first range is greater than or equal to 5, prints out False! if the length of the second range is 4, and prints out None! otherwise.
+x = [list(range(5)), list(range(5,9)), list(range(1,10,3))]
+print(len(x[0]))
+print(len(x[1]))
+if len(x[0]) >= 5:
+    print("True!")
+elif len(x[1]) == 4:
+    print("False!")
+else:
+    print("None!")
+
 
 
 
