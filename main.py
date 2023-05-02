08361143fb9fb171d548395db8607f9bc1fce23a
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-05-02 12:08:17 +0100

    Python exercise nr. 130

--- main.py
+++ main.py
@@ -967,6 +967,20 @@ else:
     print("None!")
 
 
+#  Exercise nr.130
+# Write code that prints out True! if the sum of all the elements of the first range is greater than the sum of all the elements of the third range, prints out False! if the largest element of the second range is greater than the largest element of the third range, and prints out None! otherwise.
+
+x = [list(range(5)), list(range(5,9)), list(range(1,10,3))]
+print(sum(x[0]), sum(x[2]))
+print(max(x[1]), max(x[2]))
+if sum(x[0]) > sum(x[2]):
+    print("True!")
+elif max(x[1]) > max(x[2]):
+    print("False!")
+else:
+    print("None!")
+
+
 
 
 
