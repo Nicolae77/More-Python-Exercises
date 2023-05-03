1d5af250e14baf4c53396b4471198447dbaef985
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-05-03 11:14:10 +0100

    Python exercise nr.133

--- main.py
+++ main.py
@@ -1005,5 +1005,20 @@ elif len(x[0] *2) <= sum(x[2]):
     print("False!")
 
 
+#  Exercise nr.133
+# Write code that prints out True! if the 2nd character of the value at key 1 is also present in the value at key 4, and prints out False! otherwise.
+
+x = {1: "Python", 2: "Java", 3: "Javascript", 4: "Ruby", 5: "Perl", 6: "C#", 7: "C++"}
+print(x[1][1])
+print(x[4])
+if x[1][1] in x[4]:
+    print("True!")
+else:
+    print("False!")
+
+
+
+
+
 
 
