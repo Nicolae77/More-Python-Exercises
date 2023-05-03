1f005ce93b67284b82b9e38c7f115d26f48fae93
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-05-03 11:27:44 +0100

    Python exercise nr.135

--- main.py
+++ main.py
@@ -1029,3 +1029,14 @@ else:
 
 
 
+#  Exercise nr.135
+# Write code that prints out True! if the number of characters of the smallest value in the dictionary is equal to the number of occurrences of letter a in the value at key 3, and prints out False! otherwise.
+
+x = {1: "Python", 2: "Java", 3: "Javascript", 4: "Ruby", 5: "Perl", 6: "C#", 7: "C++"}
+print(len(min(x.values())))
+print(x[3].count("a"))
+if len(min(x.values())) == x[3].count("a"):
+    print("True!")
+else:
+    print("False!")
+
