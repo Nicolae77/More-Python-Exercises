1fc3fdd8b47f0496c47b66fc10c05b99604effb1
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-05-03 11:18:24 +0100

    Python exercise nr.134

--- main.py
+++ main.py
@@ -1017,8 +1017,15 @@ else:
     print("False!")
 
 
+#  Exercise nr.134
+# Write code that prints out True! if the second to last character of the value at key 3 is the first character of the value at key 5, and prints out False! otherwise.
 
+x = {1: "Python", 2: "Java", 3: "Javascript", 4: "Ruby", 5: "Perl", 6: "C#", 7: "C++"}
 
+if x[3][-2] == x[5][0]:
+    print("True!")
+else:
+    print("False!")
 
 
 
