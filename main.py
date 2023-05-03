13568942c200d91d515536f7060ac89ecc7a184e
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-05-03 10:59:39 +0100

    Python exercise nr.132

--- main.py
+++ main.py
@@ -996,7 +996,13 @@ else:
 
 
 #  Exercise nr.132
+# Write code that prints out True! if the sum of the last 3 elements of the first range plus the sum of the last 3 elements of the 3rd range is equal to the sum of the last 3 elements of the 2nd range, and prints out False! if the length of the first range times 2 is less than the sum of all the elements of the 3rd range.
+x = [list(range(5)), list(range(5,9)), list(range(1,10,3))]
 
+if sum(x[0][-3:]) + sum(x[2][-3:]) == sum(x[1][-3:]):
+    print("True!")
+elif len(x[0] *2) <= sum(x[2]):
+    print("False!")
 
 
 
