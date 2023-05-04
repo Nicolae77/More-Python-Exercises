357ee43bbc09a4e220294d3cb6eedb5fe9e8ef1b
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-05-04 11:25:30 +0100

    Python exercise nr.145

--- main.py
+++ main.py
@@ -1118,7 +1118,16 @@ while y < 0:
     y += 1
 
 
-
+#  Exercise nr.145
+# Write a while loop that prints out the value of x times y while x is greater than or equal to 5 and less than 10, and prints out the result of x divided by y when x becomes 10.
+print('while for x and y')
+x = 5
+y = 2
+while x >= 5 and x < 10:
+    print(x * y)
+    x += 1
+else:
+    print(x / y)
 
 
 
