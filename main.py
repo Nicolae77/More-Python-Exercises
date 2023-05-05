55d3764316f8091b6cd66a0e432fcdb09a5a17c7
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-05-05 12:12:47 +0100

    Exercise nr. 148

--- main.py
+++ main.py
@@ -1143,11 +1143,18 @@ for i in y:
 x = [1, 2, 3, 4]
 y = [2, 3]
 for i in x:
-    
-    for j in y:
-        
+    for j in y: 
         print(i * j)
-        
 
+
+# #  Exercise nr.147
+# Write code that will iterate over the x and y lists and multiply each element of x with each element of y that is less than 12, also printing the results to the screen.
+x = [2, 4, 6, 8]
+y = [5, 10, 15, 20]
+
+for i in x:
+    for j in y:
+        if j < 12:
+            print(i * j)
 
 
