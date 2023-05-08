40d1b92abd7769607e9c08bee67b0286dbabd914
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-05-08 18:43:23 +0100

    Exercise nr. 154

--- main.py
+++ main.py
@@ -1200,3 +1200,41 @@ for i in my_range:
         print(i * my_range[1])
 
 
+#  Exercise nr.153
+# Write code that will iterate over the range starting at 1, up to but not including 11, with a step of 2, and for each element that is between 3 and 8 inclusively print out the result of multiplying that element by the last element in the same range. For any other element of the range (outside [3-8]) print Outside!
+my_range = range(1, 11, 2)
+for i in my_range:
+    if 3 <= i <= 8:
+        print(i * my_range[-1])
+    else:
+        print("Outside!")
+
+
+#  Exercise nr.154
+# Write code that will iterate over the range starting at 5, up to but not including 25, with a step of 5, and for each element that is between 10 and 21 inclusively print out the result of multiplying that element by the second to last element of the same range. For any other element of the range (outside [10-21]) print Outside! Finally, after the entire range is exhausted print out The end!
+
+for i in range(5, 25, 5):
+    if 10 <= i <= 21:
+        print(i * range(5, 25, 5)[-2])
+    else:
+        print("Outside!")
+else:
+    print("The end!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
