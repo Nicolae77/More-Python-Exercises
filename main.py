158a89e01dff055a8a6b70934c1e21ce9787cb85
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-05-11 11:40:47 +0100

    Exercise nr. 176

--- main.py
+++ main.py
@@ -1452,8 +1452,39 @@ print(result)
 def my_func(x, y, z):
     return x ** y + z
 
-result = my_func(2,3,4)
+result = my_func(2, 3, 4)
 print(result)
+
+
+#  Exercise nr.176
+# Implement a function called my_func() that takes a single parameter x and multiplies it with each element of range(5), also adding each multiplication result to a new (initially empty) list called my_new_list. Finally, the list should be printed out to the screen after the function is called.
+
+def my_func(x):
+    my_new_list = []
+    for i in range(5):
+        my_new_list.append(i * x)
+    return my_new_list
+
+result = my_func(2)
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
