72a87d1754218130b8a1a8a195f330e2359990de
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-06-14 10:19:42 +0100

    Python exercise nr.224

--- main.py
+++ main.py
@@ -1996,7 +1996,15 @@ result = re.findall(r"\s(o.{1})\s", s)
 
 print(result)
 
+#  Exercise nr.224
+# Write the code in order to match all the words that have at least 8 characters in the string using the findall() method.
+import re
 
+s = "Bitcoin was born on Jan 3rd 2009 as an alternative to the failure of the current financial system. In 2017, the price of 1 BTC reached $20000, with a market cap of over $300B."
+
+result = re.findall(r"\w{8,}", s)
+
+print(result)
 
 
 
