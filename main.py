73b982ff106b178962e128982adb0277829af095
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-06-16 11:26:35 +0100

    Python exercise nr.230

--- main.py
+++ main.py
@@ -2063,6 +2063,16 @@ result = re.sub(r"[0-5]", "8", s)
 print(result)
 
 
+#  Exercise nr.230
+# Write the code in order to replace all the words starting with an uppercase letter or digits greater than or equal to 6 in the string with W using the sub() method.
+
+import re
+
+s = "Bitcoin was born on Jan 3rd 2009 as an alternative to the failure of the current financial system. In 2017, the price of 1 BTC reached $20000, with a market cap of over $300B. Bitcoin, Market Cap: $184,073,529,068, Price: $10,259.02, Volume 24h: $15,670,986,269, Circulating Supply: 17,942,600 BTC, Change 24h: 0.10%"
+
+result = re.sub(r"[A-Z]\w{1,}|[6-9]", "W", s)
+
+print(result)
 
 
 
