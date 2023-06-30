aad696e5846e2b3a4cc716b4c92258d1bb53311d
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-06-30 11:00:01 +0100

    Python exercise nr. 240

--- main.py
+++ main.py
@@ -2237,3 +2237,25 @@ print(obj.p2)
 print(obj.times10(45))
 
 
+# Exercise nr.240
+# Considering the ClassOne and ClassTwo classes, write the code to verify that ClassTwo is indeed a child of ClassOne, also printing the result to the screen.
+class ClassOne(object):
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def square(self, p3):
+        print(p3 ** 2)
+
+class ClassTwo(ClassOne):
+    def times10(self, x):
+        return x * 10
+        
+print(issubclass(ClassTwo, ClassOne))
+
+
+
+
+
+
+
